baadd8ae8d2756d2be067f77e163880e5680866c
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2023-07-14 12:23:16 +0300

    added something else

---
+++ git.sh
@@ -0,0 +1,24 @@
+# New Repository
+git init
+git Status 
+git add . 
+git commit -m "Message"
+git branch -M main
+git remote add origin https://github.com/username/repName.git
+git push -u origin main
+
+# Master
+# main
+
+
+
+# Exist Repo
+
+git add .
+git commit -m "message"
+git push
+
+
+# git Configuration 
+git config --global user.email yourEmail
+git config --global user.name username
